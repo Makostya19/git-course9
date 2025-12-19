5c02cf396334acd3f9a188a6e0acfe9e3f44021f
Makostya19 <kma293385@gmail.com>
2025-12-19 15:39:45 +0900

    docs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/git-course9/",
-});
+  base: '/git-course9/',
+  build: {
+    outDir: 'docs'
+  }
+})
 
