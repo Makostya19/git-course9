af0c8841db7115ff1b8f49e6de4f579fb34e1646
Makostya19 <kma293385@gmail.com>
2025-12-31 16:08:25 +0900

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/git-course9/',
   plugins: [react()],
+  base: '/git-course9/',
 })
 
